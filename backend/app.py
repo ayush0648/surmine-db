c20421932913909fabbc78653ecc32525c4ef038
Ayushmant Bharadwaj <ayushmant.bharadwaj@gmail.com>
2024-12-16 17:35:02 +0530

    Deployment changes

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 
 CORS(app)
 
+@app.route("/")
+def serve_index():
+    return send_from_directory(app.static_folder, "index.html")
+
+@app.route("/<path:path>")
+def serve_static_files(path):
+    return send_from_directory(app.static_folder, path)
+
 # Register blueprints
 app.register_blueprint(project_files_bp, url_prefix="/project-files")
 app.register_blueprint(registrar_bp, url_prefix="/registrar")  # Add registrar blueprint
