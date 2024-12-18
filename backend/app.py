7731d25f0f06e8feb56ca7c6aeb6011fd2b32a8d
Ayushmant Bharadwaj <ayushmant.bharadwaj@gmail.com>
2024-12-18 16:47:38 +0530

    adding required libraries

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from backend.routes.project_files import project_files_bp
 from flask import send_from_directory
+import os
 
 app = Flask(__name__, static_folder='static')
 
